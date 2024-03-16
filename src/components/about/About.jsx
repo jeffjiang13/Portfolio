1e11d1a61c584e7f30b3cd3dd89f03fa8e881ba7
Jeff Jiang <jeff.jiang13@gmail.com>
2024-03-16 15:42:15 -0400

    added new project

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -8,6 +8,11 @@ import { VscFolderLibrary } from "react-icons/vsc";
 const About = () => {
   return (
     <section id="about">
+      <br />
+      <br />
+      <br />
+      <br />
+
       <h5>Get to Know</h5>
       <h2>About Me</h2>
 
